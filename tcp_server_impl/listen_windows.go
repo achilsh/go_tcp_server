7b37a8c9c5d7ef4f4286e73794dca9a183dde6e0
huwusheng <user@XP-PC-pxing>
2023-11-04 10:01:02 +0800

    add multi platform to build and run tcp server

--- tcp_server_impl/listen_windows.go
+++ tcp_server_impl/listen_windows.go
@@ -8,34 +8,17 @@ import (
 	"golang.org/x/sys/windows"
 	"log"
 	"net"
-	"runtime"
 	"syscall"
 )
 
 func (s *ServerNode) Listen() error {
-	iSysType := 0
-	sysType := runtime.GOOS
-	switch sysType {
-	case "linux":
-		iSysType = 1
-	case "windows":
-		iSysType = 2
-	default:
-		iSysType = 1
-	}
 	lc := net.ListenConfig{
 		//this control op call after param connection created and before bind op.
 		Control: func(network, address string, c syscall.RawConn) error {
 			return c.Control(func(fd uintptr) {
 				//control resuse for ip and port.
-				if iSysType == 1 {
-					syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, unix.SO_REUSEADDR, 1)
-					syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, unix.SO_REUSEPORT, 1)
-				} else if iSysType == 2 {
-					syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, windows.SO_REUSEADDR, 1)
-					syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, windows.SO_REUSEPORT, 1)
-				}
-
+				syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, windows.SO_REUSEADDR, 1)
+				syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, windows.SO_REUSEPORT, 1)
 			})
 		},
 	}
@@ -46,5 +29,4 @@ func (s *ServerNode) Listen() error {
 	}
 	s.l = r
 	return nil
-
 }
