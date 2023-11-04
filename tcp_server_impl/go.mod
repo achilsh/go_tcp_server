7de894047a1830dc889ce2e20eddf5223ef1bd8c
huwusheng <user@XP-PC-pxing>
2023-11-04 09:27:48 +0800

    set options for tcp fd on wait

--- tcp_server_impl/go.mod
+++ tcp_server_impl/go.mod
@@ -1,3 +1,5 @@
 module net_tcp_server_demo
 
 go 1.20
+
+require golang.org/x/sys v0.13.0 // indirect
--- tcp_server_impl/tcp_server.go
+++ tcp_server_impl/tcp_server.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"encoding/binary"
 	"errors"
 	"flag"
 	"fmt"
+	"golang.org/x/sys/unix"
 	"io"
 	"log"
 	"math"
@@ -111,7 +113,17 @@ func (s *ServerNode) GracefulStop() {
 	s.mu.Unlock()
 }
 func (s *ServerNode) Listen() error {
-	r, e := net.Listen(s.config.NetWork, fmt.Sprintf("%v:%d", s.config.Ip, s.config.Port))
+	lc := net.ListenConfig{
+		//this control op call after param connection created and before bind op.
+		Control: func(network, address string, c syscall.RawConn) error {
+			return c.Control(func(fd uintptr) {
+				//control resuse for ip and port.
+				syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, unix.SO_REUSEADDR, 1)
+				syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, unix.SO_REUSEPORT, 1)
+			})
+		},
+	}
+	r, e := lc.Listen(context.Background(), s.config.NetWork, fmt.Sprintf("%v:%d", s.config.Ip, s.config.Port))
 	if e != nil {
 		log.Printf("listen fail, err: %v", e)
 		return e
